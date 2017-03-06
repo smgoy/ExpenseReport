79010c97f2f999a4ef25b6ff51c5e172b87746e7
smgoy <smgoy31@gmail.com>
2017-03-06 11:43:13 -0800

    sets up user model

---
+++ app/models/user.js
@@ -0,0 +1,47 @@
+'use strict';
+var bcrypt = require('bcrypt-nodejs');
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: 8,
+          msg: "Your password must be atleast 8 characters long"
+        }
+      }
+    },
+    admin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    },
+    hooks: {
+      afterValidate: function(user) {
+        var password = user.password;
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(password, salt);
+        user.password = hash;
+      }
+    },
+    instanceMethods: {
+		    verifyPassword: function(password) {
+			       return bcrypt.compareSync(password, this.password);
+		    }
+    }
+  });
+  return User;
+};
